240deabace9bd21b9f3feedfdec62d6c76313369
Peter Peerdeman <peterpeerdeman@gmail.com>
2021-07-24 15:37:09 +0200

    chore(*): clean comments from code

--- app.py
+++ app.py
@@ -18,12 +18,6 @@ def calc():
         data = json.loads(request.data)
     except Exception:
         return jsonify(dict(message="invalid data format")), 400
-    # try:
-    #     key = data['key']
-    # except KeyError:
-    #     return jsonify(dict(message='no key submitted')), 400
-    # if key != os.environ['secret_key']:
-    #     return jsonify(dict(message='invalid key')), 400
     try:
         ds = data['ds']
         y = data['y']
@@ -34,12 +28,11 @@ def calc():
     periods = data.get('periods', p)
     m = Prophet(
             yearly_seasonality=True, 
-            # changepoints=['2020-06-12','2020-12-24', '2021-06-12']
         )
     df = pd.DataFrame(dict(ds=ds, y=y))
     m.fit(df)
     f = m.make_future_dataframe(periods=periods)
     forecast = m.predict(f)
-    # only send back new dates
+
     f_df = forecast[forecast['ds'] > df['ds'].max()].to_dict()
     return jsonify(f_df)
